68d79d32e6a93f400460004a7ebc105501e1dd01
Nikolic-Marko <marko@yoordi.com>
2024-01-10 10:34:41 +0100

    prepare for realese

--- ios/katakomTests/katakomTests.m
+++ ios/katakomTests/katakomTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface katakomTests : XCTestCase
+@interface KatakomTests : XCTestCase
 
 @end
 
-@implementation katakomTests
+@implementation KatakomTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
